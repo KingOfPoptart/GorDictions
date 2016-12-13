c977534d2816e203e7b9a5a2465f9d4e8e3a1b86
Richard Duarte <richard.c.duarte@gmail.com>
2016-12-13 17:41:02 +0000

    Initial Commit

---
+++ GorDictions.py
@@ -0,0 +1,22 @@
+from random import randint
+
+addictions = [ "midget hookers playing football",
+"touching yourself while eating sardines",
+"cutting your hands off and surgically reattaching them to your butt",
+"golden potato salad",
+"dick salad",
+"screaming I AM A SILVER SKELETOR",
+"being a brony",
+"mastering the art of shaving your own butthole",
+"teaching mayonnaise how to love",
+"grand piano sexual chocolate",
+"ALCOHOL!  Collect your death on the way out",
+"weird foot fetish porn (again)",
+"bigger blacker dicks",
+"coming up with sexual versions of children songs",
+"calling white people ma nigga"
+  ]
+
+
+print "Your new addiction is.... "+ addictions[randint(0, len(addictions)-1)] + "!"
+print "Congratulations!"
